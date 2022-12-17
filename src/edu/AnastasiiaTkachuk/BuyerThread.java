b2178bcd3a44ab9e08913370289bd4ff9adadf86
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2022-12-17 20:35:19 +0200

    Initial commit

---
+++ src/edu/AnastasiiaTkachuk/BuyerThread.java
@@ -0,0 +1,37 @@
+package edu.AnastasiiaTkachuk;
+
+import java.util.Queue;
+
+public class BuyerThread implements Runnable{
+    private final Queue<Cashbox> cashboxes;
+
+    public BuyerThread(Queue<Cashbox> cashboxes) {
+        this.cashboxes = cashboxes;
+    }
+
+    @Override
+    public void run() {
+        try {
+            synchronized (cashboxes){
+                while (true){
+                    if(!cashboxes.isEmpty()) {
+                        Cashbox cashbox = cashboxes.remove();
+                        System.out.println(Thread.currentThread().getName() + " обслуживается в классе " + cashbox);
+
+                        cashboxes.wait(5L);
+
+                        System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashbox);
+                        cashboxes.add(cashbox);
+                        cashboxes.notifyAll();
+                        break;
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + " ожидает свободную кассу");
+                        cashboxes.wait();
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
